6a0c024cf286a18896b36420d309b99c79d66cc3
Felipe Bis <felipe.bis@aluno.ifsp.edu.br>
2020-11-06 19:28:40 -0300

    Ordenacao dos pokemons para a listagem em ordem

--- scripts/scriptPokedexV2.js
+++ scripts/scriptPokedexV2.js
@@ -1,206 +1,198 @@
-class Pokedex{
+class Pokedex {
     constructor() {
-        this.TodosPoke = []
-        this.offset = 0
-        this.limit = 10
-        this.baseURL = 'https://pokeapi.co/api/v2/pokemon/'
-        this.DivLoading = document.querySelector("#load")
-        this.DivError = document.querySelector("#error")
+        this.TodosPoke = [];
+        this.offset = 0;
+        this.limit = 10;
+        this.baseURL = "https://pokeapi.co/api/v2/pokemon/";
+        this.DivLoading = document.querySelector("#load");
+        this.DivError = document.querySelector("#error");
     }
 
     //Carregar os pokemons
-    async loadPokemons(){
-        
+    async loadPokemons() {
         const urlPokemons = `${this.baseURL}?limit=${this.limit}&offset=${this.offset}`;
 
         const { data } = await axios.get(urlPokemons);
 
-        this.DivLoading.classList.add('invisivel')
+        this.DivLoading.classList.add("invisivel");
 
-        const NomeDoPokemaos = data.results.map(({name}) => name)
-        
+        const NomeDoPokemaos = data.results.map(({ name }) => name);
+
         await this.funcListarPoke(NomeDoPokemaos);
-        
 
-        this.offset = this.offset + this.limit
+        this.offset = this.offset + this.limit;
     }
 
     //Para a listagem dos pokes
-    async funcListarPoke(pokemonNames){
-        
-        const LugarDosPoke = document.querySelector('[id="TodoMundo"]')
-       
-        pokemonNames.forEach(async(pokemonName) => {
-        
-            const pokemon = await this.funcGetPokemon(pokemonName)
+    async funcListarPoke(pokemonNames) {
+        const LugarDosPoke = document.querySelector('[id="TodoMundo"]');
 
-            const types = pokemon.types.map(({type}) => type.name);
+        for (let i = 0; i < pokemonNames.length; i++) {
+            await this.funcGetPokemon(pokemonNames[i]).then((pokemon) => {
+                const types = pokemon.types.map(({ type }) => type.name);
+                const listElement = document.createElement("li");
+                listElement.classList.add(`card`);
+                listElement.classList.add(`${types[0]}1`);
+                listElement.setAttribute("id", `poke${pokemon.id}`);
 
-            //const types1 = ajustarType(types[0])
-            //const types2 = ajustarType(types[1])
-            
-
-            //console.log(types[0].charAt(0).toUpperCase() + types[0].slice(1))
-            const listElement = document.createElement('li');
-            listElement.classList.add(`card`);
-            listElement.classList.add(`${types[0]}1`)
-            listElement.setAttribute('id', `poke${pokemon.id}`);
-    
-            listElement.innerHTML = `
+                listElement.innerHTML = `
             
                 <div class="infos">
                     <h3 class="NumDoPoke">#${pokemon.id}</h3>
                     <h2 class="NomeDoPoke">${pokemon.name}</h2>
                     <div class="Tipos">
                         <span class="badge ${types[0]}">
-                            <img class="ImagemDoTipo" src="styles/types/${types[0]}.svg"/>
-                        ${types.join(` </span> <span class="badge ${types[1]}"> <img class="ImagemDoTipo" src="styles/types/${types[1]}.svg"/>`)}</span>
+                            <img class="ImagemDoTipo" src="styles/types/${
+                                types[0]
+                            }.svg"/>
+                        ${types.join(
+                            ` </span> <span class="badge ${types[1]}"> <img class="ImagemDoTipo" src="styles/types/${types[1]}.svg"/>`
+                        )}</span>
                     </div>
                 </div>
                 
                 <div class="ImagemPoke">
                     <img class="fundoPoke" src="styles/images/bg.png">
-                    <img class="imagem" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png"/>
+                    <img class="imagem" alt="${
+                        pokemon.name
+                    }" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                    pokemon.id
+                }.png"/>
                 </div>
-            `;    
-            //
-            //<img class="imagem" alt="${pokemonNomeFormatado}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png"/>
-            //<img class="imagem" alt="${pokemonNomeFormatado}" src="https://raw.githubusercontent.com/jnovack/pokemon-svg/master/svg/${pokemon.id}.svg"/>
-            listElement.addEventListener('click', () => funcDetalhes(pokemon.id));
-    
-            LugarDosPoke.appendChild(listElement);
-        });
-    
+            `;
+
+                listElement.addEventListener("click", () =>
+                    funcDetalhes(pokemon.id)
+                );
+
+                LugarDosPoke.appendChild(listElement);
+            });
+        }
     }
 
     //Criando a url para os pokemao
-    async  funcGetPokemon(idOrName){
-        
-        const urlDoPokemao = 'https://pokeapi.co/api/v2/pokemon/'+idOrName
+    async funcGetPokemon(idOrName) {
+        const urlDoPokemao = "https://pokeapi.co/api/v2/pokemon/" + idOrName;
 
-        this.DivLoading.classList.remove('invisivel')
-
-        const { data } = await axios.get(urlDoPokemao)
-        
-        this.DivLoading.classList.add('invisivel')
+        this.DivLoading.classList.remove("invisivel");
+        let { data } = await axios.get(urlDoPokemao);
+        this.DivLoading.classList.add("invisivel");
 
         return data;
     }
 }
 
 //Criando URL para a proxima evolucao
-async function funcGetPokemonEvolution(idOrName){
-    
-    const urlDoPokemao = 'https://pokeapi.co/api/v2/pokemon/'+idOrName
+async function funcGetPokemonEvolution(idOrName) {
+    const urlDoPokemao = "https://pokeapi.co/api/v2/pokemon/" + idOrName;
     const { data } = await axios.get(urlDoPokemao);
     return data.id;
-    
 }
 
 //Pagina Inicial
-function funcPaginainicial(){
-    window.location = '/';
+function funcPaginainicial() {
+    window.location = "/";
 }
 
 //Se eu quiser ajustar as letras
-function ajustarNome(NomeDoPokemao){
+function ajustarNome(NomeDoPokemao) {
     return NomeDoPokemao.charAt(0).toUpperCase() + NomeDoPokemao.slice(1);
 }
 
 //Funcao que faz a busca dos pokemons
-async function funcBuscarOsPoke(){
-    const campoBusca = document.getElementById('campo-busca');
-    const pokedex = new Pokedex()
+async function funcBuscarOsPoke() {
+    const campoBusca = document.getElementById("campo-busca");
+    const pokedex = new Pokedex();
     const keyword = campoBusca.value.trim();
 
-    if(keyword !== ''){
-        const UlDosPokes = document.querySelector('ul#TodoMundo')
-        
-        UlDosPokes.innerHTML="";
+    if (keyword !== "") {
+        const UlDosPokes = document.querySelector("ul#TodoMundo");
+
+        UlDosPokes.innerHTML = "";
 
         await pokedex.funcListarPoke([keyword]);
-        
     }
 }
 
 //Func mostrar status
-function funcMostrarStatus(){
-    var divTudoStatus  = document.getElementById('TudosobreStatus').style.display
+function funcMostrarStatus() {
+    var divTudoStatus = document.getElementById("TudosobreStatus").style
+        .display;
 
-    if(divTudoStatus == "block"){
-        document.getElementById('TudosobreStatus').style.display = "none"
-    }
-    else{
-        document.getElementById('TudosobreStatus').style.display = "block"
-        document.getElementById('Tudosobre').style.display = "none"
+    if (divTudoStatus == "block") {
+        document.getElementById("TudosobreStatus").style.display = "none";
+    } else {
+        document.getElementById("TudosobreStatus").style.display = "block";
+        document.getElementById("Tudosobre").style.display = "none";
     }
 }
 
 //Func mostrar sobre
-function funcMostrarSobre(){
-    var divTudoSobre = document.getElementById('Tudosobre').style.display
+function funcMostrarSobre() {
+    var divTudoSobre = document.getElementById("Tudosobre").style.display;
 
-    if(divTudoSobre == "block"){
-        document.getElementById('Tudosobre').style.display = "none"
-    }
-    else{
-        document.getElementById('Tudosobre').style.display = "block"
-        document.getElementById('TudosobreStatus').style.display = "none"
+    if (divTudoSobre == "block") {
+        document.getElementById("Tudosobre").style.display = "none";
+    } else {
+        document.getElementById("Tudosobre").style.display = "block";
+        document.getElementById("TudosobreStatus").style.display = "none";
     }
 }
 
 //Buscar informacoes gerais
-async function buscarInfoEvolucao(urlEvolucao, Poke){
-    var Evolution = []
+async function buscarInfoEvolucao(urlEvolucao, Poke) {
+    var Evolution = [];
     //const urlEvolucao = 'https://pokeapi.co/api/v2/evolution-chain/'+idOrName
     //console.log(urlEvolucao)
     const { data } = await axios.get(urlEvolucao);
-    if(data.chain.evolves_to != "")
-        Evolution.push(data.chain.evolves_to[0].species.name)
-    else
-        Evolution.push(data.chain.species.name)
-    return Evolution
+    if (data.chain.evolves_to != "")
+        Evolution.push(data.chain.evolves_to[0].species.name);
+    else Evolution.push(data.chain.species.name);
+    return Evolution;
 }
 
 //GET URL da evolucao
-async function GetUrlEvolution(idOrName){
-    const urlSobrePoke = 'https://pokeapi.co/api/v2/pokemon-species/'+idOrName
-    const { data } = await axios.get(urlSobrePoke)
-    return data.evolution_chain.url
+async function GetUrlEvolution(idOrName) {
+    const urlSobrePoke =
+        "https://pokeapi.co/api/v2/pokemon-species/" + idOrName;
+    const { data } = await axios.get(urlSobrePoke);
+    return data.evolution_chain.url;
 }
 
 //Descrição ajustavel
 function ajustarDesc(desc) {
-    let content = desc.toString().replace(/\f/g, ' ').split('\f');
+    let content = desc.toString().replace(/\f/g, " ").split("\f");
     return content;
 }
 
 //Buscar informacoes sobre evolucao
-async function buscarInfoSobre(idOrName){
-    const urlSobrePoke = 'https://pokeapi.co/api/v2/pokemon-species/'+idOrName
-    const { data } = await axios.get(urlSobrePoke)
+async function buscarInfoSobre(idOrName) {
+    const urlSobrePoke =
+        "https://pokeapi.co/api/v2/pokemon-species/" + idOrName;
+    const { data } = await axios.get(urlSobrePoke);
     return data.flavor_text_entries[6].flavor_text;
 }
 
 //Buscar Genero Pokemon
-async function buscarInfoGenero(idOrName){
-    const urlSobrePoke = 'https://pokeapi.co/api/v2/pokemon-species/'+idOrName
-    const { data } = await axios.get(urlSobrePoke)
-    return data.genera[7].genus
+async function buscarInfoGenero(idOrName) {
+    const urlSobrePoke =
+        "https://pokeapi.co/api/v2/pokemon-species/" + idOrName;
+    const { data } = await axios.get(urlSobrePoke);
+    return data.genera[7].genus;
 }
 
 //Buscar Pelo tipo de fraqueza
-async function buscarFraqueza(NameType){
-    const urlSobrePoke = 'https://pokeapi.co/api/v2/type/'+NameType
-    const { data } = await axios.get(urlSobrePoke)
-    return data.damage_relations.double_damage_from
+async function buscarFraqueza(NameType) {
+    const urlSobrePoke = "https://pokeapi.co/api/v2/type/" + NameType;
+    const { data } = await axios.get(urlSobrePoke);
+    return data.damage_relations.double_damage_from;
 }
 
 //Buscar pelos status do pókemon
-async function buscarStatus(idOrName){
-    const urlSobrePoke = 'https://pokeapi.co/api/v2/pokemon/'+idOrName
-    const { data } = await axios.get(urlSobrePoke)
-    return data.stats
+async function buscarStatus(idOrName) {
+    const urlSobrePoke = "https://pokeapi.co/api/v2/pokemon/" + idOrName;
+    const { data } = await axios.get(urlSobrePoke);
+    return data.stats;
 }
 
 //Funcao do detalhes dos pokemons
@@ -211,16 +203,15 @@ async function funcDetalhes(Poke){
 
     document.write('<head>')
     document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">')
-    document.write('<link rel="stylesheet" href="styles/stylePokedex.css">')
+    document.write('<link rel="stylesheet" href="styles/reset.css"/>')
+    document.write('<link rel="stylesheet" href="styles/stylePokedexV2.css">')
     document.write('</head>')
 
     document.write('<body class="PaginaToda">')
 
-    document.write('<div class="BarrinhaBonita">')
-    document.write('<h3 class="TituloBarrinha">A melhor pokédex do Brasil!</h3>')
+    document.write('<header class="BarrinhaBonita">')
     document.write('<img class="Loguinho" onclick="funcPaginainicial()"src="styles/images/1200px-International_Pokémon_logo.svg.png">')
-    document.write('<h5 class="Autor">by: Felipe Bis</h5>')
-    document.write('</div>')
+    document.write('</header>')
 
     document.write('<div class="BarrinhaDeBusca">')
     document.write('<button id="voltar" style="border-radius: 7px;" onclick="funcPaginainicial()" class="back" type="button">Voltar</button>')
@@ -248,23 +239,29 @@ async function funcDetalhes(Poke){
 
     const { id, name } = pokemon;
 
-    const pokemonNomeFormatado = ajustarNome(name);
 
     const informacoesFraqueza = await buscarFraqueza(types[0])
     
     const Fraquezas = informacoesFraqueza.map(({name}) => name)
     
     document.write(`
-        <li id="poke${id}" class="pokes${types[0]} Detalhes">
-        <img class="fundoPoke" src="styles/images/bg.png"/>
-        <img class="imagem ${types[0]}" alt="${pokemonNomeFormatado}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png"/>
+        <li id="poke${id}" class="card ${types[0]}1 Detalhes">
+
+        <div class="ImagemPoke">
+                    <img class="fundoPoke" src="styles/images/bg.png">
+                    <img class="imagem" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png"/>
+                </div>
+
+        <div class="infos">
             <h3 class="NumDoPoke">#${id}</h3>
-            <h2 class="NomeDoPoke">${pokemonNomeFormatado}</h2>
+            <h2 class="NomeDoPoke">${name}</h2>
             
             <div class=Tipos>
-                <span class="Tipos${types[0]}"><img class="ImagemDoTipo" src="styles/types/${types[0]}.svg"/>
-                ${types.join(` </span> <span class="Tipos${types[1]}"> <img class="ImagemDoTipo" src="styles/types/${types[1]}.svg"/>`)}</span>
+                <span class="badge ${types[0]}">
+                <img class="ImagemDoTipo" src="styles/types/${types[0]}.svg"/>
+                ${types.join(` </span> <span class="badge ${types[1]}"> <img class="ImagemDoTipo" src="styles/types/${types[1]}.svg"/>`)}</span>
             </div>
+        </div>
 
             <div id="botoesDetalhes">
                 <button id="sobre" onclick="funcMostrarSobre()">About</button>
@@ -295,38 +292,28 @@ async function funcDetalhes(Poke){
                   
     document.write('</ul>')
     document.write('</div>')
-    document.write('<div class="LogoPokemao">')
-    document.write('<img class="ImagemFim" src="styles/images/pokedex-logo.png">')
-    document.write('</div>')
     document.write('</body>')
     document.write('</html>')
 }
 
 //Adicionando evento de busca no formulario (input)
-const formBusca = document.getElementById('formulario-busca');
-formBusca.addEventListener('submit', (event) => {
+const formBusca = document.getElementById("formulario-busca");
+formBusca.addEventListener("submit", (event) => {
     event.preventDefault();
 
     funcBuscarOsPoke();
-
-})
+});
 
 //Função de quando a tela se inicia - minha MAIN
-window.onload = function(){
+window.onload = function () {
+    const pokedex = new Pokedex();
 
-    const pokedex = new Pokedex()
-
-    pokedex.loadPokemons()
-
-    //Quando o scroll chegar ao final
-   document.querySelector('div.Pokemaos').addEventListener('scroll',function(){
-        if(this.scrollTop + this.offsetHeight == (this.scrollHeight)){
-            pokedex.loadPokemons()
-        }
-    })
-}
-
-
-
-
-
+    pokedex.loadPokemons();
+    document
+        .querySelector("div.Pokemaos")
+        .addEventListener("scroll", function () {
+            if (this.scrollTop + this.offsetHeight == this.scrollHeight) {
+                pokedex.loadPokemons();
+            }
+        });
+};
